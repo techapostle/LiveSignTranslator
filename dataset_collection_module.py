8876da759c3ed5d67f320cb293b4a109a8496285
Kian Dadkhah <contact.kiands@gmail.com>
2022-08-12 17:42:29 -0400

    explicitly import libraries instead of using ones from other files

--- dataset_collection_module.py
+++ dataset_collection_module.py
@@ -1,16 +1,21 @@
+import cv2
+import numpy as np
+import time
+import os
 from string import ascii_lowercase
 import action_detection_module as ad
 
+
 class ModelTrainer():
     def __init__(self) -> None:
-        self.DATA_PATH = ad.os.path.join('MP_Data')
+        self.DATA_PATH = os.path.join('MP_Data')
         arr = []
         for i in range(0, 10):
             arr.append(str(i))
         for c in ascii_lowercase:
             arr.append(c)
         # Actions that we try to detect
-        self.actions = ad.np.array(arr)
+        self.actions = np.array(arr)
         # Thirty videos worth of data
         self.no_sequences = 30
         # Videos are going to be 30 frames in length
@@ -22,7 +27,7 @@ class ModelTrainer():
         for action in self.actions:
             for sequence in range(1, self.no_sequences+1):
                 try:
-                    ad.os.makedirs(ad.os.path.join(
+                    os.makedirs(os.path.join(
                         self.DATA_PATH, action, str(sequence)))
                 except:
                     pass
@@ -31,16 +36,16 @@ class ModelTrainer():
 def main():
     modelTrainer = ModelTrainer()
     modelTrainer.createDirectories()
-    cap = ad.cv2.VideoCapture(0)
-    holistic = ad.ActionDetector(
+    cap = cv2.VideoCapture(0)
+    holistic = ActionDetector(
         min_detection_confidence=0.5, min_tracking_confidence=0.5)
     exit = False
 
     for action in modelTrainer.actions:
-        
+
         # Loop through sequences aka videos
         start = False
-        
+
         for sequence in range(modelTrainer.start_folder, modelTrainer.start_folder+modelTrainer.no_sequences):
             if exit:
                 break
@@ -51,40 +56,41 @@ def main():
                 # Make detections
                 image, results = holistic.mediapipe_detection(
                     frame, holistic.holistic)
-                image = ad.cv2.cvtColor(image, ad.cv2.COLOR_BGR2RGB)
+                image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
                 # Draw landmarks
                 holistic.draw_landmarks_styled(image, results)
                 # NEW Apply wait logic
                 if frame_num == 0:
-                    ad.cv2.putText(image, 'STARTING COLLECTION', (120, 200),
-                                   ad.cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 4, ad.cv2.LINE_AA)
-                    ad.cv2.putText(image, 'Collecting frames for {} Video Number {}'.format(action, sequence), (15, 12),
-                                   ad.cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1, ad.cv2.LINE_AA)
+                    cv2.putText(image, 'STARTING COLLECTION', (120, 200),
+                                   cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 4, cv2.LINE_AA)
+                    cv2.putText(image, 'Collecting frames for {} Video Number {}'.format(action, sequence), (15, 12),
+                                   cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1, cv2.LINE_AA)
                     # Show to screen
-                    ad.cv2.imshow('OpenCV Feed', image)
-                    ad.cv2.waitKey(2000)
+                    cv2.imshow('OpenCV Feed', image)
+                    cv2.waitKey(2000)
                 else:
-                    ad.cv2.putText(image, 'Collecting frames for {} Video Number {}'.format(action, sequence), (15, 12),
-                                   ad.cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1, ad.cv2.LINE_AA)
+                    cv2.putText(image, 'Collecting frames for {} Video Number {}'.format(action, sequence), (15, 12),
+                                   cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1, cv2.LINE_AA)
                     # Show to screen
-                    ad.cv2.imshow('OpenCV Feed', image)
+                    cv2.imshow('OpenCV Feed', image)
                 # NEW Export keypoints
                 keypoints = holistic.extract_landmarks(results)
-                npy_path = ad.os.path.join(
+                npy_path = os.path.join(
                     modelTrainer.DATA_PATH, action, str(sequence), str(frame_num))
-                ad.np.save(npy_path, keypoints)
+                np.save(npy_path, keypoints)
                 # Break gracefully
-                if ad.cv2.waitKey(10) & 0xFF == ord('q'):
+                if cv2.waitKey(10) & 0xFF == ord('q'):
                     exit = True
                     break
 
         if exit:
             break
         while start == False:
-            if ad.cv2.waitKey(10) & 0xFF == ord('s'):
+            if cv2.waitKey(10) & 0xFF == ord('s'):
                 start = True
     cap.release()
-    ad.cv2.destroyAllWindows()
+    cv2.destroyAllWindows()
 
 
 main()
+
